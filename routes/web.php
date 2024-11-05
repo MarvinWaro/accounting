fa4da1798324cd15691e658d3ed3e55ab7fc5daf
MarvinWaro <mrvnwr.dev@gmail.com>
2024-11-05 12:57:40 +0800

    running admin-dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+
+    Route::get('accounting_dashboard', function () {
+        return view('accounting.accounting_dashboard');
+    })->name('accounting_dashboard');
+
+    Route::get('transaction', function () {
+        return view('accounting.transaction');
+    })->name('transaction');
+
+
+
 });
