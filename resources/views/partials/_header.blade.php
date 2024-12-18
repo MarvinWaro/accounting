777b298599fd2effb7162ce15a6a87307a51e923
MarvinWaro <mrvnwr.dev@gmail.com>
2024-12-18 11:26:03 +0800

    adding jquery

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -30,6 +30,16 @@
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@9.0.3"></script>
         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
+        <!-- Include jQuery -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+        <!-- Select2 CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+        <!-- Select2 JS -->
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+
     </head>
 
 <body class="font-sans antialiased">
